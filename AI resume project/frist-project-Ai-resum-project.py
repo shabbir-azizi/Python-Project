f47d47d09e1f844c53e9933d83df4a3cbc402de7
unknown <azizishabbir355@gmail.com>
2026-07-05 16:10:30 +0500

    commit changes inside project

--- AI resume project/frist-project-Ai-resum-project.py
+++ AI resume project/frist-project-Ai-resum-project.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect, session
 from db import base, engine, sessionlocal
 import models
+import PyPDF2
+import docx
+import json
 
 app = Flask(__name__)
+
+
+app.secret_key="secret123"
+
+base.metadata.create_all(bind=engine)
 
 
 @app.route('/')                                                
