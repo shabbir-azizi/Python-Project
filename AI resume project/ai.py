601ee01ee9807f6b882c0b34f7b46d696cf1ac51
unknown <azizishabbir355@gmail.com>
2026-07-09 14:52:35 +0500

    commit changes inside project

--- AI resume project/ai.py
+++ AI resume project/ai.py
@@ -12,7 +12,21 @@ Evaluate the resume based on the user's goal.
 User goal:" {user_goal}"
 
 STRICT RULES:
+- Extract only relevant skills for this goal
+- REMOVE irrelevant tools [excel for backend,etc]
+- Idsentify real gaps
+- Generate roadmap only for missing fields
+- make sure to provide a clear and concise summary of the resume's strengths and weaknesses in relation to the user's goal.M    
+- Make output DIFFERENT based on goal 
 
+
+Return only JSON:
+{{
+"skills":[],
+"missing_skills":[],
+roadmap":[],
+
+}}
 
 
 """
