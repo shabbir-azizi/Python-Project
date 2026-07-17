ce4125d2b6191d5282a324a1ccb99cbe39b56bf2
unknown <azizishabbir355@gmail.com>
2026-07-17 16:35:18 +0500

    commit changes inside project

--- AI resume project/second-project-Ai-resume-project.py
+++ AI resume project/second-project-Ai-resume-project.py
@@ -17,4 +17,26 @@ class Report(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     resume_text = Column(Text)
     user_goal = Column(Text)
+    result = Column(Text) 
+
+
+    from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    resume_text = Column(Text)
+    user_goal = Column(Text)
     result = Column(Text)
